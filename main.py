3560cb336af19fac47a845e62a97e6e3d1f9c431
HelloTanvir <tanvirhossain.butex@gmail.com>
2023-01-21 20:26:08 +0600

    file upload functionality added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # run uvicorn main:app --reload to start the server
 
 import os
+from typing import List
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, responses
+from fastapi import FastAPI, HTTPException, UploadFile, responses
 
 load_dotenv()
 
@@ -25,6 +26,7 @@ def say_hello(name: str = user_name):
             <h3>Hello {name}</h3>
             {name == user_name and hint_msg or info_msg}
             <button onclick="location.href='/html'" {name == user_name and "disabled" or ''}>Click me</button>
+            <a href="/upload">Upload file</a>
         """
     )
 
@@ -37,3 +39,29 @@ def about():
 @app.get("/html")
 def html():
     return responses.FileResponse("./template/index.html")
+
+
+@app.get("/upload")
+def upload():
+    return responses.FileResponse("./template/upload.html")
+
+
+@app.post("/upload-file")
+def upload_file(file: UploadFile):
+    directory_path = './uploads'
+
+    print(file.filename)
+    if file.filename is not None:
+        print(file.filename)
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+
+    with open(f'{directory_path}/{file.filename}', "wb") as f:
+        f.write(file.file.read())
+
+    return responses.RedirectResponse(url=f"/see-file/?file={directory_path}/{file.filename}", status_code=302)
+
+
+@app.get("/see-file")
+def see_file(file: str):
+    return responses.FileResponse(file)
